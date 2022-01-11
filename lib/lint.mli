f5831f5210e15e23b9ced3fe0671f0d018415256
Marek Kubica <marek@tarides.com>
2022-01-11 11:37:07 +0100

    Add unit test to exercise the validation logic

--- lib/lint.mli
+++ lib/lint.mli
@@ -1,2 +1,3 @@
+val check_version : string -> (unit, [ `Msg of string ]) result
 val check_dune_project : string -> (unit, [ `Msg of string ]) result
 val opam_uses_dune : string -> (unit, [ `Msg of string ]) result
